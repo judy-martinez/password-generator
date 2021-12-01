5b2572985012229e98765cb4ad29e332ea4e1ee6
Judy <judy.mtz1@gmail.com>
2021-11-30 21:16:59 -0600

    fixed parseInt

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,14 +20,14 @@ function writePassword() {
   } else if (enter <8 || enter > 128) {
       // Validates user input
       // Start user input prompts
-      enter = parseint(prompt("You must choose between 8 and 128 characters"));
+      enter = parseInt(prompt("You must choose between 8 and 128 characters"));
 
   } else {
       //Goes on when user input is validated
       confirmLowercase = confirm("Would you like to include lowercase characters?");
       confirmUppercase = confirm("Would you like to include uppercase characters?");
       confirmNumber = confirm("Would you like to include numbers?");
-      confirmCharacter = confirm("Would you like to include special characters?")
+      confirmCharacter = confirm("Would you like to include special characters?");
   };
 
   // password variable is an array placeholder for user generated amount of length
