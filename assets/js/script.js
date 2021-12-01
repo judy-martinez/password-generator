5f588ec7bfaefc08178ae2ec80580ba5d9e66fc7
Judy <judy.mtz1@gmail.com>
2021-11-30 20:10:57 -0600

    added confirm lowercase input variable

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@
 
 //Input variables:
 var enter;
+var confirmLowercase;
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -12,11 +13,15 @@ function writePassword() {
   enter = parseInt(prompt("How long would you like your password to be? Choose between 8 and 128 characters"));
   // If statement
   if (!enter) {
-      alert("How many characters long would you like your password to be?");
+      alert("You must type in a value. How many characters would you like in your password?");
   } else if (enter <8 || enter > 128) {
       // Validates user input
       // Start user input prompts
-      enter = parseint(prompt("You must choose between 8 and 128"));
+      enter = parseint(prompt("You must choose between 8 and 128 characters"));
+
+  } else {
+      //Goes on when user input is validated
+      confirmLowercase = confirm("Would you like to include lowercase characters?");
   };
 
   var password = generatePassword();
