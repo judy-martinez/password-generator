5dd5583c50c7f66a368d0d558e98a5b32fa5e62b
Judy <judy.mtz1@gmail.com>
2021-11-30 20:07:23 -0600

    add enter variable

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,24 @@
 // Assignment code here
 
+//Input variables:
+var enter;
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  // User Input
+  enter = parseInt(prompt("How long would you like your password to be? Choose between 8 and 128 characters"));
+  // If statement
+  if (!enter) {
+      alert("How many characters long would you like your password to be?");
+  } else if (enter <8 || enter > 128) {
+      // Validates user input
+      // Start user input prompts
+      enter = parseint(prompt("You must choose between 8 and 128"));
+  };
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
@@ -14,4 +27,7 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", function (){
+  ps = writePassword();
+  document.getElementsByClassName("password").placeholder = ps;
+});
