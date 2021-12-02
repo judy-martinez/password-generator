429b8086af97afc327ff26b5b6d9d299446d4533
Judy <judy.mtz1@gmail.com>
2021-12-01 22:02:14 -0600

    parsing integers in user input for pw length

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,9 +20,11 @@ function writePassword() {
 // Generate password function
 function generatePassword() {
   // Prompts user Input
-  var pwLength = prompt(
-    "How long would you like your password to be? Choose between 8 and 128 characters"
-  );
+  var pwLength = prompt("How long would you like your password to be? Choose between 8 and 128 characters");
+  // Parses integers in user input for pw length
+  var lengthConfirm = parseInt(pwLength);
+  console.log(lengthConfirm);
+
   // Choose length of password b/w 8 & 128. First if statement.
   if (pwLength < 8 || pwLength > 128 || isNaN(pwLength)) {
     alert("Error. Please enter a value between 8 and 128.");
@@ -31,15 +33,16 @@ function generatePassword() {
     // Start user input prompts
   } 
   
-//paste variables here
 // Split is a string function that splits the string of characters instead of typing everything out in array
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/split
 var collectionOfChar = [];
-var confirmLowercase = "abcdefghiklmnopqrsttuvwxtz".split("");
-var confirmUppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
-var confirmNumber = "0123456789".split("");
-var confirmCharacter = "*&^%$#@!?><{}".split("");
+var lowerCase = "abcdefghiklmnopqrsttuvwxtz".split("");
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+var numberInt = "0123456789".split("");
+var specialChar = "*&^%$#@!?><{}".split("");
   
 }
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+//*ask about parseInt*
